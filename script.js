df7410acd9e13463801bb68a0e20210bf0071f28
TZ <tz116917@gmail.com>
2021-10-04 18:55:29 -0700

    search history js completed

--- script.js
+++ script.js
@@ -4,22 +4,11 @@ console.log("hey")
 
 function pastSearch(location){
     console.log("1")
-    var searched = document.getElementsByClassName("searched")
-    for (i=0;i<searched.length;i++){
-        searched[i].addEventListener("click", function(event){event.preventDefault()})
-        console.log("added"+i)
-    }
     
-
-    
-    url="https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid=7a35162ff8a1ab9814d70cac9d534864"
+    var url="https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid=7a35162ff8a1ab9814d70cac9d534864"
     console.log("pastseach: "+url)
-    fetch(url)
-        
-        .then(function (response) {
-
-          return response.json();
-        })
+    
+    Display(url)
 
 }
 
@@ -33,12 +22,18 @@ function searchInput(){
     console.log(inputText.value)
     
     
-    url1="https://api.openweathermap.org/data/2.5/weather?q="+inputText.value+"&appid=7a35162ff8a1ab9814d70cac9d534864"
+    var url="https://api.openweathermap.org/data/2.5/weather?q="+inputText.value+"&appid=7a35162ff8a1ab9814d70cac9d534864"
     console.log(url)
     
     Display(url)
 
-    Create
+    var createCard = document.createElement("button")
+    createCard.setAttribute("class", "searched")
+    createCard.textContent = inputText.value
+    createCard.addEventListener("click", function(){pastSearch(createCard.textContent)})
+    console.log(createCard)
+
+    searchList.appendChild(createCard)
 
 }
 
@@ -53,17 +48,5 @@ function Display(url){
         .then(function (data) {
             console.log("data",data)
             
-            
-
-}
-
-function createHistory(){
-var createCard = document.createElement("section")
-            createCard.setAttribute("class", "searched")
-            createCard.textContent = inputText.value
-            createCard.addEventListener("click", pastSearch(createCard.textContent))
-            console.log(createCard)
-
-            searchList.appendChild(createCard)
-
+        })
 }
