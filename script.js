36eb18101a9ade3ebdc27868401b708979ea22f5
TZ <tz116917@gmail.com>
2021-10-04 18:18:52 -0700

    search function in js

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 var searchList = document.getElementById("searchlist")
 
-function start(){
+console.log("hey")
 
+function pastSearch(location){
+    console.log("1")
+    var searched = document.getElementsByClassName("searched")
+    for (i=0;i<searched.length;i++){
+        searched[i].addEventListener("click", function(event){event.preventDefault()})
+        console.log("added"+i)
+    }
+    
+
+    
+    url="https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid=7a35162ff8a1ab9814d70cac9d534864"
+    console.log("pastseach: "+url)
+    fetch(url)
+        
+        .then(function (response) {
+
+          return response.json();
+        })
+
+}
+
+function searchInput(){
+
+
+    var form1 = document.getElementById("form1")
+    form1.addEventListener("submit", function(event){event.preventDefault()})
 
     var inputText = document.getElementById("inputtext")
     console.log(inputText.value)
     
-    url="https://api.openweathermap.org/data/2.5/weather?q="+inputText.value+"&appid=7a35162ff8a1ab9814d70cac9d534864"
+    
+    url1="https://api.openweathermap.org/data/2.5/weather?q="+inputText.value+"&appid=7a35162ff8a1ab9814d70cac9d534864"
     console.log(url)
+    
+    Display(url)
+
+    Create
+
+}
+
+function Display(url){
+
     fetch(url)
         
         .then(function (response) {
@@ -17,23 +53,17 @@ function start(){
         .then(function (data) {
             console.log("data",data)
             
-            var createCard = document.createElement("section")
+            
+
+}
+
+function createHistory(){
+var createCard = document.createElement("section")
             createCard.setAttribute("class", "searched")
             createCard.textContent = inputText.value
+            createCard.addEventListener("click", pastSearch(createCard.textContent))
             console.log(createCard)
 
             searchList.appendChild(createCard)
 
-            
-            
-            }
-          );
-      
-    
-
 }
-
-
-
-
-
